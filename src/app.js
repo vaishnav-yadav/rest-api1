2ecab98b50b6162dbb381cec3a78d03ed82ef598
Vaishnav Yadav <70229716+vaishnav-yadav@users.noreply.github.com>
2020-12-24 23:52:19 +0530

    get request of one student data successfully showing data according to id

--- src/app.js
+++ src/app.js
@@ -71,8 +71,16 @@ app.get('/students/:id' , async(req, res)=>{
     try{
         const _id= req.params.id;
         const studentData= await Student.findById({_id: _id});
+
+        if(!studentData){
+            return res.status(404).send();
+        }
+        else
+        {
+
+            res.send(studentData);
+        }
         // console.log(req.params);
-        res.send(studentData);
     }catch(e){
         console.log(e);
     }

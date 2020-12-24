64a837ec88abf4d3cf9f42da8c3e65449a81e44d
Vaishnav Yadav <70229716+vaishnav-yadav@users.noreply.github.com>
2020-12-24 22:30:30 +0530

    Implemented post request functionality and now we can post userdata directly into the mongodb database

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('./db/conn'); //requiring the conn.js file 
+const Student = require('./models/students');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('hello from the other sides, so you have made a get request ')
+})
+
+
+//now we are adding results directly by posting it from postman by making post request
+
+
+app.post('/students', (req, res) => {
+   
+    console.log(req.body);
+    const user = new Student(req.body); //getting post request here of storing data in Student database. Getting student data in user
+
+    //below code will store the user data that was posted in our database mongodb
+    user.save().then(()=>{
+        res.status(201).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+    // res.send('hello user you have made a post request');
+});
+
+app.listen(port, ()=>{
+    console.log(  `connection is set up at ${port}`);
+})
---
+++ src/db/conn.js
@@ -0,0 +1,13 @@
+//connection 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/students-api', {
+    useCreateIndex: true, 
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+}).then(() =>{
+    console.log('connection is successful...')
+}).catch((e)=>{
+           console.log('No connection');
+})
+
